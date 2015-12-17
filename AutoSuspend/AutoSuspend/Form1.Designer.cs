f083f1c536c1baa67244c3158de71a036eefea90
Timoral <atimson123@gmail.com>
2015-12-17 17:00:47 +0000

    Inital

---
+++ AutoSuspend/AutoSuspend/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AutoSuspend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonNow = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(12, 9);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(264, 13);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = "Click the button to stop the timer or the pc will suspend";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Location = new System.Drawing.Point(12, 230);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(280, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop Timer and Close";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Location = new System.Drawing.Point(100, 85);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(89, 13);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "Time Remaining: ";
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.labelTimer);
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 170);
+            this.panel1.TabIndex = 3;
+            // 
+            // buttonNow
+            // 
+            this.buttonNow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNow.Location = new System.Drawing.Point(12, 201);
+            this.buttonNow.Name = "buttonNow";
+            this.buttonNow.Size = new System.Drawing.Size(280, 23);
+            this.buttonNow.TabIndex = 3;
+            this.buttonNow.Text = "Suspend now";
+            this.buttonNow.UseVisualStyleBackColor = true;
+            this.buttonNow.Click += new System.EventHandler(this.buttonNow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 265);
+            this.Controls.Add(this.buttonNow);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "Form1";
+            this.Text = "Auto Suspend";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonNow;
+    }
+}
+
---
+++ AutoSuspend/AutoSuspend/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AutoSuspend
+{
+    public partial class Form1 : Form
+    {
+        private static int time;
+
+
+        public Form1(string timeToLive)
+        {
+            InitializeComponent();
+            time = int.Parse(timeToLive);
+            labelTimer.Location = new Point((panel1.Width / 2) - (labelTimer.Width / 2), (panel1.Height / 2) - (labelTimer.Height / 2));
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTimer.Text = "Time Remaining: " + time.ToString();
+            labelTimer.Location = new Point((panel1.Width / 2) - (labelTimer.Width / 2), (panel1.Height / 2) - (labelTimer.Height / 2));
+            if (time == 0)
+            {
+                timer1.Stop();
+                //MessageBox.Show("Suspend!");
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+                Application.Exit();
+            }
+            time--;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Application.Exit();
+        }
+
+        private void buttonNow_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Application.SetSuspendState(PowerState.Suspend, true, true);
+            Application.Exit();
+        }
+    }
+}
